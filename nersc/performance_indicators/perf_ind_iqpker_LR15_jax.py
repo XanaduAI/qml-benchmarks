07b49a956833ded8af7bbc30ddb7f2c1027ffe41
Joseph Bowles <bowles.physics@gmail.com>
2024-04-16 15:38:14 +0200

    update code

--- nersc/performance_indicators/perf_ind_iqpker_LR15_jax.py
+++ nersc/performance_indicators/perf_ind_iqpker_LR15_jax.py
@@ -7,24 +7,38 @@ import time
 import csv
 import os
 import yaml
-
-from qml_benchmarks.models.iqp_kernel import IQPKernelClassifier
+import subprocess
 from qml_benchmarks.hyperparam_search_utils import read_data
 
+#################################
+# settings for the performance indicator.
+# You only need to change this to make a different performance indicator
+
+from qml_benchmarks.models.iqp_kernel import IQPKernelClassifier as Model
+use_jax = True
+vmap = True
+jit = True
+perf_ind_name = 'JAX'  #a name for the performance indicator used for naming files
+n_features = 3 #dataset dimension
+n_train = 50 #number of data points to fit model
+n_test = 50 #number of datapoints for prediction
+
+#################################
+
+model_name = Model().__class__.__name__
+
+# get the 'worst case' hyperparameter settings for the model (those that require the most resources)
 with open('hyperparam_settings.yaml', "r") as file:
     hp_settings = yaml.safe_load(file)
 
-hyperparams = {**hp_settings['IQPKernelClassifier'], **{'use_jax':True, 'vmap':True}}
-
+hyperparams = {**hp_settings[model_name], **{'use_jax':use_jax, 'vmap':vmap, 'jit': jit}}
 print(hyperparams)
-
-n_features = 15 #dataset dimension
 
 X_train,y_train = read_data(f'../../paper/benchmarks/linearly_separable/linearly_separable_{n_features}d_train.csv')
 X_test,y_test = read_data(f'../../paper/benchmarks/linearly_separable/linearly_separable_{n_features}d_train.csv')
 
-model = IQPKernelClassifier(**hyperparams)
-model.fit(X_train[:50], y_train[:50])
+model = Model(**hyperparams)
+model.fit(X_train[:n_train], y_train[:n_train])
 
 #kernel construction time
 construct_kernel_time = model.construct_kernel_time_
@@ -32,23 +46,35 @@ construct_kernel_time = model.construct_kernel_time_
 training_time = model.training_time_
 #prediction time
 time0 = time.time()
-model.predict(X_test[:50])
+model.predict(X_test[:n_test])
 predict_time = time.time() - time0
 
 
 #write to csv
 data = [construct_kernel_time, training_time, predict_time, hyperparams]
 
-model_name = model.__class__.__name__
-filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_JAX.csv"
+filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_{perf_ind_name}.csv"
+packages_filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_{perf_ind_name}_packages.txt"
+scontrol_filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_{perf_ind_name}_scontrol.txt"
+
 header = ['construct_kernel_time', 'training_time', 'predict_time', 'hyperparameters']
 
-if not os.path.exists('JAX'):
+if not os.path.exists(perf_ind_name):
     # Create the directory
-    os.mkdir('JAX')
+    os.mkdir(perf_ind_name)
 
-with open('JAX/'+filename, mode="w", newline="") as file:
+#write perf indicator data
+with open(perf_ind_name+'/'+filename, mode="w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(header)
     for row in [data]:
         writer.writerow(row)
+
+# get package list and write to file
+output = subprocess.check_output(['pip', 'list']).decode('utf-8')
+with open(perf_ind_name+'/'+packages_filename, 'w') as file:
+    file.write(output)
+
+# make an empty text file to store the scontrol data
+with open(perf_ind_name+'/'+scontrol_filename, 'w') as file:
+    pass
--- nersc/performance_indicators/perf_ind_iqpvar_LR15_jax.py
+++ nersc/performance_indicators/perf_ind_iqpvar_LR15_jax.py
@@ -7,19 +7,32 @@ import time
 import csv
 import os
 import yaml
-
-from qml_benchmarks.models.iqp_variational import IQPVariationalClassifier
+import subprocess
 from qml_benchmarks.hyperparam_search_utils import read_data
 
+#################################
+# settings for the performance indicator.
+# You only need to change this to make a different performance indicator
+
+from qml_benchmarks.models.iqp_variational import IQPVariationalClassifier as Model
+use_jax = True
+vmap = True
+jit = True
+max_steps = 5 #the number of gradient descent steps to use to estimate the step time
+perf_ind_name = 'JAX'  #a name for the performance indicator used for naming files
+n_features = 3 #dataset dimension
+n_trials = 5 #number of trials to average over
+
+#################################
+
+model_name = Model().__class__.__name__
+
+# get the 'worst case' hyperparameter settings for the model (those that require the most resources)
 with open('hyperparam_settings.yaml', "r") as file:
     hp_settings = yaml.safe_load(file)
 
-hyperparams = {**hp_settings['IQPVariationalClassifier'], **{'use_jax':True, 'vmap':True, 'max_steps':10}}
-
+hyperparams = {**hp_settings[model_name], **{'use_jax':use_jax, 'vmap':vmap, 'max_steps':max_steps, 'jit': jit}}
 print(hyperparams)
-
-n_features = 15 #dataset dimension
-n_trials = 5 #number of trials to average over
 
 X_train,y_train = read_data(f'../../paper/benchmarks/linearly_separable/linearly_separable_{n_features}d_train.csv')
 X_test,y_test = read_data(f'../../paper/benchmarks/linearly_separable/linearly_separable_{n_features}d_train.csv')
@@ -31,7 +44,9 @@ predict_times = []
 for trial in range(n_trials):
     jax.clear_caches()
 
-    model = IQPVariationalClassifier(**hyperparams)
+    model = Model(**hyperparams)
+
+    #note we train for max_steps only, so we won't reach convergence.
     model.fit(X_train, y_train)
 
     #get step times from loss history data
@@ -50,7 +65,6 @@ for trial in range(n_trials):
 
 
 #calculate mean and stds
-
 first_train_step = np.mean(first_train_steps)
 first_train_step_std = np.std(first_train_steps)
 
@@ -64,17 +78,30 @@ predict_time_std = np.std(predict_times)
 data = [first_train_step, first_train_step_std, consec_train_step, consec_train_step_std, predict_time,
         predict_time_std, hyperparams]
 
-model_name = model.__class__.__name__
-filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_JAX.csv"
+filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_{perf_ind_name}.csv"
+packages_filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_{perf_ind_name}_packages.txt"
+scontrol_filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_{perf_ind_name}_scontrol.txt"
+
 header = ['first_train_step', 'first_train_step_std', 'consec_train_step', 'consec_train_step_std', 'predict_time',
           'predict_time_std', 'hyperparameters']
 
-if not os.path.exists('JAX'):
+if not os.path.exists(perf_ind_name):
     # Create the directory
-    os.mkdir('JAX')
+    os.mkdir(perf_ind_name)
 
-with open('JAX/'+filename, mode="w", newline="") as file:
+#write perf indicator data
+with open(perf_ind_name+'/'+filename, mode="w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(header)
     for row in [data]:
         writer.writerow(row)
+
+# get package list and write to file
+output = subprocess.check_output(['pip', 'list']).decode('utf-8')
+with open(perf_ind_name+'/'+packages_filename, 'w') as file:
+    file.write(output)
+
+# make an empty text file to store the scontrol data
+with open(perf_ind_name+'/'+scontrol_filename, 'w') as file:
+    pass
+
