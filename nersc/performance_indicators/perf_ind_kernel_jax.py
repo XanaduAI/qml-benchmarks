c380d43abf865ee13cb3b54551ad1e38e667c87f
Jan Balewski <janstar1122@gmail.com>
2024-04-30 15:29:24 -0700

    reads data but crash in Python scalars

--- nersc/performance_indicators/perf_ind_kernel_jax.py
+++ nersc/performance_indicators/perf_ind_kernel_jax.py
@@ -10,76 +10,105 @@ import yaml
 import subprocess
 from qml_benchmarks.hyperparam_search_utils import read_data
 
-print(os.getcwd())
-os.chdir('performance_indicators')
+import argparse
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v","--verbosity",type=int,choices=[0, 1, 2,3,4],  help="increase output verbosity", default=1, dest='verb')
+    parser.add_argument("--inputPath",default='../linearly_separable',help='input data location')
+    parser.add_argument('-n','--numFeatures',type=int,default=2, help="dataset dimension ")
 
-#################################
-# settings for the performance indicator.
-# You only need to change this to make a different performance indicator
+    args = parser.parse_args()
+    
+    print( 'myArg-program:',parser.prog)
+    for arg in vars(args):  print( 'myArg:',arg, getattr(args, arg))
 
-#define the model
-from qml_benchmarks.models.iqp_kernel import IQPKernelClassifier as Model
+    #assert os.path.exists(args.outPath)
+    return args
 
-#implementation attributes of model
-use_jax = True
-vmap = True
-jit = True
+#=================================
+#=================================
+#  M A I N 
+#=================================
+#=================================
+if __name__=="__main__":
+    args=get_parser()
 
-perf_ind_name = 'JAX'  #a name for the performance indicator used for naming files
-n_features = 2  #dataset dimension
+    print(os.getcwd())
+    os.chdir('performance_indicators')
 
-#################################
+    #################################
+    # settings for the performance indicator.
+    # You only need to change this to make a different performance indicator
 
-model_name = Model().__class__.__name__
+    #define the model
+    from qml_benchmarks.models.iqp_kernel import IQPKernelClassifier as Model
 
-# get the 'worst case' hyperparameter settings for the model (those that require the most resources)
-with open('hyperparam_settings.yaml', "r") as file:
-    hp_settings = yaml.safe_load(file)
+    #implementation attributes of model
+    use_jax = True
+    vmap = True
+    jit = True
 
-hyperparams = {**hp_settings[model_name], **{'use_jax':use_jax, 'vmap':vmap, 'jit': jit}}
-print(hyperparams)
+    perf_ind_name = 'JAX'  #a name for the performance indicator used for naming files
+    n_features = args.numFeatures  #dataset dimension
 
-X_train,y_train = read_data(f'../../paper/benchmarks/linearly_separable/linearly_separable_{n_features}d_train.csv')
-X_test,y_test = read_data(f'../../paper/benchmarks/linearly_separable/linearly_separable_{n_features}d_train.csv')
+    #################################
 
-model = Model(**hyperparams)
-model.fit(X_train, y_train)
+    model_name = Model().__class__.__name__
 
-#kernel construction time
-construct_kernel_time = model.construct_kernel_time_
-#full training time
-training_time = model.training_time_
-#prediction time
-time0 = time.time()
-model.predict(X_test)
-predict_time = time.time() - time0
+    # get the 'worst case' hyperparameter settings for the model (those that require the most resources)
+    with open('hyperparam_settings.yaml', "r") as file:
+        hp_settings = yaml.safe_load(file)
+
+    hyperparams = {**hp_settings[model_name], **{'use_jax':use_jax, 'vmap':vmap, 'jit': jit}}
+    print(hyperparams)
+    assert os.path.exists(args.inputPath)
+    #inpF1=f'../../paper/benchmarks/linearly_separable/linearly_separable_{n_features}d_train.csv'
+    inpF1=os.path.join(args.inputPath,'linearly_separable_%dd_train.csv'%(n_features))
+    inpF2=inpF1.replace('train','test')
+    print('M:inpF1',inpF1)
+    X_train,y_train = read_data(inpF1)
+    print('M:inpF2',inpF2)
+    X_test,y_test = read_data(inpF2)
+
+    model = Model(**hyperparams)
+    model.fit(X_train, y_train)
+
+    #kernel construction time
+    construct_kernel_time = model.construct_kernel_time_
+    #full training time
+    training_time = model.training_time_
+    #prediction time
+    time0 = time.time()
+    model.predict(X_test)
+    predict_time = time.time() - time0
 
 
-#write to csv
-data = [construct_kernel_time, training_time, predict_time, hyperparams]
+    #write to csv
+    data = [construct_kernel_time, training_time, predict_time, hyperparams]
 
-filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_{perf_ind_name}.csv"
-packages_filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_{perf_ind_name}_packages.txt"
-scontrol_filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_{perf_ind_name}_scontrol.txt"
+    filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_{perf_ind_name}.csv"
+    packages_filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_{perf_ind_name}_packages.txt"
+    scontrol_filename =  model_name+f"_linearly_separable_{n_features}d_performance_indicators_{perf_ind_name}_scontrol.txt"
 
-header = ['construct_kernel_time', 'training_time', 'predict_time', 'hyperparameters']
+    header = ['construct_kernel_time', 'training_time', 'predict_time', 'hyperparameters']
 
-if not os.path.exists(perf_ind_name):
-    # Create the directory
-    os.mkdir(perf_ind_name)
+    if not os.path.exists(perf_ind_name):
+        # Create the directory
+        os.mkdir(perf_ind_name)
 
-#write perf indicator data
-with open(perf_ind_name+'/'+filename, mode="w", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerow(header)
-    for row in [data]:
-        writer.writerow(row)
+    #write perf indicator data
+    with open(perf_ind_name+'/'+filename, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        for row in [data]:
+            writer.writerow(row)
 
-# get package list and write to file
-output = subprocess.check_output(['pip', 'list']).decode('utf-8')
-with open(perf_ind_name+'/'+packages_filename, 'w') as file:
-    file.write(output)
+    # get package list and write to file
+    output = subprocess.check_output(['pip', 'list']).decode('utf-8')
+    with open(perf_ind_name+'/'+packages_filename, 'w') as file:
+        file.write(output)
 
-# make an empty text file to store the scontrol data
-with open(perf_ind_name+'/'+scontrol_filename, 'w') as file:
-    pass
+    # make an empty text file to store the scontrol data
+    with open(perf_ind_name+'/'+scontrol_filename, 'w') as file:
+        pass
+    print('M:done')
--- nersc/wrap_podman2.sh
+++ nersc/wrap_podman2.sh
@@ -16,19 +16,23 @@ fi
 echo W:BASE_DIR=$BASE_DIR
 echo 'W:start podman'
 podman-hpc run -it \
-	   --volume $HOME:/home \
-	   --volume $CFSH/$BASE_DIR:/$BASE_DIR \
-	   --volume $CFSH/$WORK_DIR:$WORK_DIR \
-	   -e HDF5_USE_FILE_LOCKING='FALSE' \
-	   --workdir $WORK_DIR \
-	   $IMG <<EOF 
-cd $BASE_DIR
+    --volume $CFSH/$BASE_DIR:/root \
+    --volume $CFSH/$BASE_DIR:$BASE_DIR \
+    --volume $CFSH/$BASE_DIR/linearly_separable:/linearly_separable \
+    --volume $CFSH/$WORK_DIR:$WORK_DIR \
+    -e HDF5_USE_FILE_LOCKING='FALSE' \
+    --workdir $WORK_DIR \
+    $IMG <<EOF 
 echo P:pwd; pwd
-pip3 install .
-cd -
+echo P:all
+ls -l ../..
 $CMD
 echo P:done
 exit
 EOF
   
 echo 'W:done podman'
+
+# spare
+# 	   --volume $HOME:/home \
+    # --volume  $CFSH/qml-benchmarks:/root 
